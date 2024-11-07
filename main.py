e762dc53c0ea37a9d6608cb19a39af1d0ed4568e
Jaek19 <promise20203@naver.com>
2024-11-07 22:45:52 +0900

    py 2번쨰수정

--- main.py
+++ main.py
@@ -70,8 +70,7 @@ async def get_waiting_animals():
             
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
-
+""" 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.1", port=8000) """
