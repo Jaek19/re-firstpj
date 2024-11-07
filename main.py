ab862451ad922bdc0077cba7073248db234a814d
Jaek19 <promise20203@naver.com>
2024-11-07 22:41:20 +0900

    py수정

--- main.py
+++ main.py
@@ -73,4 +73,5 @@ async def get_waiting_animals():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
